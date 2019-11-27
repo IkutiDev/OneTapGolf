22961c56171093030edf60d069d18f4021bee6af
Ikuti <m.j.olkiewicz@gmail.com>
2019-11-27 22:52:07 +0100

    Cleaned up folder structure, created a method for random hole spawning

---
+++ One Tap Golf/Assets/Scripts/HoleMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleMovement : MonoBehaviour
+{
+    public void MoveHoleToRandomPosition()
+    {
+        var newPosition = Random.Range(0f, 7.7f);
+        transform.position=new Vector3(newPosition,transform.position.y);
+    }
+}
