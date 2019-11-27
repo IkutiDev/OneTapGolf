34e38270eda9c8ed653548c5be3ac5ca103f2322
Ikuti <m.j.olkiewicz@gmail.com>
2019-11-27 23:10:36 +0100

    Created simple score class, added in readonly inspector values

---
+++ One Tap Golf/Assets/PointsCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Utils;
+
+public class PointsCounter : MonoBehaviour
+{
+    [SerializeField, ReadOnly] private int score;
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void SetScore(int newScore)
+    {
+        score = newScore;
+        gameObject.GetComponent<Text>().text = score.ToString();
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        gameObject.GetComponent<Text>().text = score.ToString();
+    }
+}
--- One Tap Golf/Assets/Scripts/TestGameManager.cs
+++ One Tap Golf/Assets/Scripts/TestGameManager.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class TestGameManager : MonoBehaviour
 {
     [SerializeField] private HoleMovement hole;
+
+    [SerializeField] private PointsCounter score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score.SetScore(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.N))
+        if (Input.GetKeyDown(KeyCode.N)) // [N]ext level
         {
             hole.MoveHoleToRandomPosition();
-        }   
+        }
+        else if (Input.GetKeyDown(KeyCode.M)) // [M]ore score
+        {
+            score.IncrementScore();
+        }
     }
 }
