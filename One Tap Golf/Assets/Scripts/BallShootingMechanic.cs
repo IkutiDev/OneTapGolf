295ea88a823b6c4de88bd8391222f10b54da802e
Ikuti <m.j.olkiewicz@gmail.com>
2019-11-29 23:18:00 +0100

    Created a script for golf ball shooting with empty methods and debug logs for now

---
+++ One Tap Golf/Assets/Scripts/BallShootingMechanic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class BallShootingMechanic : MonoBehaviour
+{
+    [SerializeField] private GameObject trajectoryDot;
+    private float powerOfProjectile;
+    [SerializeField]private float projectilePowerIncreaseIncrement;
+
+    private void Start()
+    {
+        powerOfProjectile = 0f;
+        //projectilePowerIncreaseIncrement = 0.1f;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            powerOfProjectile += projectilePowerIncreaseIncrement;
+            ShowProjectileTrajectory();
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            ShootProjectile();
+        }
+    }
+
+    private void ShootProjectile()
+    {
+        Debug.Log("Kapow!");
+        powerOfProjectile = 0f;
+    }
+
+    private void ShowProjectileTrajectory()
+    {
+        Debug.Log(powerOfProjectile);
+    }
+}
